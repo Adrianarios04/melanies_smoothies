14c0f9b67bf676d4d00abe733fb27e464a9966a6
Adrianarios04 <146380834+Adrianarios04@users.noreply.github.com>
2026-07-22 22:39:35 -0600

    Update streamlit_app.py
    Add Search_On lookup

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,11 @@ my_dataframe = session.table(
     col("FRUIT_NAME"),
     col("SEARCH_ON")
 )
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
+
+# Convert the Snowpark DataFrame to a Pandas DataFrame
+pd_df = my_dataframe.to_pandas()
 
 st.dataframe(
     data=my_dataframe,
@@ -49,6 +54,19 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+
+        search_on = pd_df.loc[
+            pd_df["FRUIT_NAME"] == fruit_chosen,
+            "SEARCH_ON"
+        ].iloc[0]
+
+        st.write(
+            "The search value for ",
+            fruit_chosen,
+            " is ",
+            search_on,
+            "."
+        )
 
         st.subheader(fruit_chosen + ' Nutrition Information')
 
